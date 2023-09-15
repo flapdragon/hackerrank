1d6f28a58c97e903a3444ed8c917ee7b24ab175a
david <vermicious.knid.d@gmail.com>
2023-09-15 15:09:56 -0700

    Initial commit. Adds code from HR and README.

---
+++ interview-preparation-kit/warm-up-challenges/jumping-on-the-clouds/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// const fs = require('fs');
+
+// process.stdin.resume();
+// process.stdin.setEncoding('utf-8');
+
+// let inputString = '';
+// let currentLine = 0;
+
+// process.stdin.on('data', function(inputStdin) {
+//     inputString += inputStdin;
+// });
+
+// process.stdin.on('end', function() {
+//     inputString = inputString.split('\n');
+
+//     main();
+// });
+
+// function readLine() {
+//     return inputString[currentLine++];
+// }
+
+/*
+ * Complete the 'jumpingOnClouds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY c as parameter.
+ */
+
+function jumpingOnClouds(c) {
+  console.log("Ah might as well jump. Jump!")
+}
+
+jumpingOnClouds()
+
+// function main() {
+//     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+//     const n = parseInt(readLine().trim(), 10);
+
+//     const c = readLine().replace(/\s+$/g, '').split(' ').map(cTemp => parseInt(cTemp, 10));
+
+//     const result = jumpingOnClouds(c);
+
+//     ws.write(result + '\n');
+
+//     ws.end();
+// }
